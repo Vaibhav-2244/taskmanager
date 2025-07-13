0ba7149b354ea3dd0e169434d93f41e0065ca64c
vaibhav-2244 <vaibhavbhargava05@gmail.com>
2025-07-13 17:50:18 +0530

    Fix render.yaml and add gunicorn

--- app.py
+++ app.py
@@ -195,4 +195,5 @@ def delete_task(task_id):
         conn.close()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
+
--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-# config.py
+import os
 
 DB_CONFIG = {
-    'server': 'taskmanager-1234.database.windows.net',
-    'database': 'taskmanagerdb',
-    'username': 'taskadmin',
-    'password': 'task@2025', 
+    'server': os.environ.get('DB_SERVER'),
+    'database': os.environ.get('DB_NAME'),
+    'username': os.environ.get('DB_USER'),
+    'password': os.environ.get('DB_PASS'),
     'driver': '{ODBC Driver 18 for SQL Server}'
 }
